a2daa449c0ce84cc45197a0642ad22c39323b865
soyoung01 <5040unng@gmail.com>
2025-05-28 04:37:35 +0900

    🙈 chore: 수정된 gitignore 적용

--- app/auth/security.py
+++
@@ -1,9 +0,0 @@
-from fastapi import Depends, HTTPException, status
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-
-security = HTTPBearer(auto_error=False) 
-
-def get_token(credentials: HTTPAuthorizationCredentials = Depends(security)) -> str:
-    if not credentials or not credentials.scheme.lower() == "bearer":
-        raise HTTPException(status_code=401, detail="Authorization 헤더가 필요합니다.")
-    return credentials.credentials
