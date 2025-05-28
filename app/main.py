1b52ee218c09aad45e5a9f927858f3d0d416053f
soyoung01 <5040unng@gmail.com>
2025-05-29 00:37:33 +0900

    👷 build: CORS 설정

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import chat
 
 app = FastAPI()
+
+#CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://mefoweb.com",
+        "https://api.mefoweb.com",
+        "https://care.mefoweb.com",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(chat.router)
 
